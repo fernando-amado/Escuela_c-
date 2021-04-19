c57ebf2345e5bbf04440c80856a9037aa48c5eae
fernado-amado <lfamado84@misena>
2021-04-19 14:17:49 -0600

    Crud periodos api tester

--- SSV2/Controllers/PeriodoesController.cs
+++ SSV2/Controllers/PeriodoesController.cs
@@ -7,24 +7,19 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using SSV2.Models;
 
 namespace SSV2.Controllers
 {
-    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class PeriodoesController : ApiController
     {
         private SSDBV2Container db = new SSDBV2Container();
 
         // GET: api/Periodoes
-        public IQueryable GetPeriodoes()
+        public IQueryable<Periodo> GetPeriodoes()
         {
-            var list = (from peri in db.Periodoes
-                        select new { Id=peri.Id ,
-                        Nombre=peri.NombreP});
-            return list;
+            return db.Periodoes;
         }
 
         // GET: api/Periodoes/5
---
+++ SSV2/Views/Front/assets/JavaScript/Periodos.js
@@ -0,0 +1,97 @@
+const tabla = document.querySelector(".tbody");
+const boton = document.getElementById("ButtonAdd");
+const inputNombre = document.getElementById("nombre");
+const inputId = document.getElementById("idPeriodo");
+const nombreEditar = document.getElementById("nombreEditar");
+const btnGuardarMateria = document.getElementById("ButtonAddEditar");
+const Porcentaje
+boton.addEventListener("click", () => {
+	Agregar(inputNombre.value);
+});
+
+btnGuardarMateria.addEventListener("click", () => {
+	Editar(inputId.value, nombreEditar.value);
+});
+
+function listarMateria() {
+	fetch("https://localhost:44351/api/Materias")
+		.then((response) => response.json())
+		.then((materias) =>
+			materias.forEach((materia) => {
+				llenarTabla(materia);
+			})
+		);
+}
+
+function llenarTabla(m) {
+	let nMateria = document.createElement("tr");
+
+	nMateria.innerHTML += "<td>" + m.Nombre + "</td>";
+	nMateria.setAttribute("data-id", m.Id);
+	nMateria.innerHTML += `<td class="tdBoton "><button class="buttonEditar far fa-edit"onclick="AbrirEditar(${m.Id},'${m.Nombre}')">Editar</button>
+    <button class=" fas fa-trash-alt buttonEliminar" onclick="Eliminar(${m.Id})">Eliminar</button></td>`;
+	tabla.appendChild(nMateria);
+	inputNombre.value = "";
+}
+
+function Agregar(m) {
+	fetch("https://localhost:44351/api/Materias", {
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json"
+		},
+		method: "POST",
+		body: JSON.stringify({
+			Nombre: m
+		})
+	})
+		.then((response) => response.json())
+		.then((data) => llenarTabla(data));
+}
+
+function AbrirEditar(id, nombre) {
+	OpenUpdate();
+	inputId.value = id;
+	nombreEditar.value = nombre;
+}
+
+function Editar(id, nombre) {
+	fetch("https://localhost:44351/api/Materias/" + id, {
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json"
+		},
+		method: "PUT",
+		body: JSON.stringify({
+			Id: parseInt(id),
+			Nombre: nombre
+		})
+	}).then(() => {
+		let tr = document.querySelector(`tr[data-id="${id}"]`);
+
+		tr.innerHTML = `<td>${nombre}</td><td class="tdBoton "><button class="buttonEditar far fa-edit"onclick="AbrirEditar(${id},'${nombre}')">Editar</button>
+    <button class=" fas fa-trash-alt buttonEliminar" onclick="Eliminar(${id})">Eliminar</button></td>`;
+	}),
+		limpiarDatos(),
+		CloseUpdate();
+}
+
+function Eliminar(id) {
+	fetch("https://localhost:44351/api/Materias/" + id, {
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json"
+		},
+		method: "DELETE",
+		body: JSON.stringify({
+			Id: parseInt(id)
+		})
+	}).then(() => {
+		let tr = document.querySelector(`tr[data-id="${id}"]`);
+		tabla.removeChild(tr);
+		inputId.value = "";
+		inputNombre.value = "";
+	});
+}
+
+listarMateria();
